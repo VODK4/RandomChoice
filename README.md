288c6a14569c64f87f9905e19ca51ee86ef5fc45
The Vodka <hasanpoyrazsahin@gmail.com>
2021-12-12 17:11:06 +0300

    Update 2.1
    Quality improvements, better input method and new mode.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
  Make a list and get an elemenet out randomly.
 
 All items are seperated by ",".
+
+1.0:
+    Basic app runs on terminal.
+
+2.0:
+    -Added framework.
+    -Easier use.
+
+2.1:
+    -Changed entry to text widget for easier user interaction.
+    -Now users are able to use program multiple times without the need of reboot.
+    -Added new mode. (Allow item only once.)
+        +It prevent you from putting same element multiple times by acident.
--- main.py
+++ main.py
@@ -11,26 +11,38 @@ def elementCountList():
     entryName.config(bg="#F2B90C")
     entryName.pack()
 
-    ınputBox = Entry(root)
+    ınputBox = Text(root, height=5, width=70)
     ınputBox.pack()
 
-    def textToList():
-        entry = ınputBox.get()
+    def submit():
+        entry = ınputBox.get("1.0", "end-1c")
         ınput = entry.split(",")
 
         output = random.choice(ınput)
+        OUTPUT.clear()
         OUTPUT.insert(0, output)
 
         OUTPUTList = str(OUTPUT)
 
+        if cb1Var.get() == 1:
+            ınput = str(list(dict.fromkeys(ınput)))
+
         outputLabel.config(text="Output="+OUTPUTList)
 
 
+    cb1 = Checkbutton(root, text="Allow item only once.")
+    cb1.config(bg="#F2B90C")
 
-    Button(root, text="Submit Input", command=textToList).pack()
+    cb1Var = IntVar()
+    cb1.config(variable=cb1Var)
+    cb1.config(onvalue=1, offvalue=0)
+    cb1.pack()
+
+    Button(root, text="Submit Input", command=submit).pack()
 
     
     outputLabel = Label(root)
+    outputLabel.config(bg="#F2B90C")
     outputLabel.pack()
 
 
